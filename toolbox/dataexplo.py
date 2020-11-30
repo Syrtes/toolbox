1ee9faecb91424fc6a4887c0167a04c582a68d6c
Syrtes <alexandre.galicier@gmail.com>
2020-11-30 15:41:50 +0100

    fixed issues on plot and kwargs

--- toolbox/dataexplo.py
+++ toolbox/dataexplo.py
@@ -1,15 +1,15 @@
 # Import from standard library
 import matplotlib.pyplot as plt
 import seaborn as sns
+from statsmodels.graphics.gofplots import qqplot
 
-def distribution_plot(numerical_features):
-    """Plot the distribution for a list of numerical features"""
-    for numerical_feature in numerical_features:
-        fig, ax =plt.subplots(1,3,figsize=(20,8))
+def distribution_plot(df, numerical_features):
+    """Plot the distributions for a list of numerical features from a dataframe df"""
+    for feature in features:
+        fig, ax =plt.subplots(1,3,figsize=(20,5))
         ax[0].set_title(f"Distribution of: {numerical_feature}")
-        sns.histplot(data = numerical_data[numerical_feature], kde=True, ax=ax[0])
+        sns.histplot(data = df[feature], kde=True, ax=ax[0])
         ax[1].set_title(f"Boxplot of: {numerical_feature}")
-        sns.boxplot(numerical_data[numerical_feature], ax=ax[1])
+        sns.boxplot(x = df[feature], ax=ax[1])
         ax[2].set_title(f"Gaussianity of: {numerical_feature}")
-        qqplot(numerical_data[numerical_feature],line='s',ax=ax[2])
-        fig.show()
+        qqplot(data = df[feature],line='s',ax=ax[2])
